7db373c078a7ee004fcf3ec6e21cdce9953a508a
Olivier Van hamme <nicefiction@gmail.com>
2021-08-12 17:35:24 +0200

    Removes the Add and Remove card implementation in the ContentView .

--- Memorize2021/View/ContentView.swift
+++ Memorize2021/View/ContentView.swift
@@ -25,55 +25,19 @@ struct ContentView: View {
    
    var body: some View {
       
-      return VStack {
-         ScrollView {
-            LazyVGrid(columns: [GridItem(.adaptive(minimum: 75))]) {
-               ForEach(emojis[0..<cardCount],
-                       id: \.self) { (emoji: String) in
-                  CardView(content: emoji)
-                     .aspectRatio(2/3,
-                                  contentMode: .fit)
-               }
+      ScrollView {
+         LazyVGrid(columns: [GridItem(.adaptive(minimum: 75))]) {
+            ForEach(emojis[0..<cardCount],
+                    id: \.self) { (emoji: String) in
+               CardView(content: emoji)
+                  .aspectRatio(2/3,
+                               contentMode: .fit)
             }
          }
-         .foregroundColor(.red)
-//         Spacer()
-//         HStack {
-//            subtractCard
-//               .disabled(cardCount == 1 ? true : false)
-//               .opacity(cardCount == 1 ? 0.65 : 1.00)
-//            Spacer()
-//            Text("\(cardCount) \(cardCount == 1 ? "Card" : "Cards")")
-//               .font(.callout)
-//            Spacer()
-//            addCard
-//               .disabled(cardCount == emojis.count ? true : false)
-//               .opacity(cardCount == emojis.count ? 0.65 : 1.00)
-//         }
-//         .font(.largeTitle)
-//         .padding(.horizontal)
       }
+      .foregroundColor(.red)
+      .padding(.horizontal)
    }
-   
-   
-//   var addCard: some View {
-//
-//      return Button(action: {
-//         cardCount += 1
-//      }, label: {
-//         Image(systemName: "plus.square")
-//      })
-//   }
-//
-//
-//   var subtractCard: some View {
-//
-//      return Button(action: {
-//         cardCount -= 1
-//      }, label: {
-//         Image(systemName: "minus.square")
-//      })
-//   }
 }
 
 
